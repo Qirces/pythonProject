dd705800e4e9aa55159337f3c54b5690f6d9ca3f
Дмитрий Щербанев <DAShherbanev@stud.kpfu.ru>
2022-11-09 22:27:27 +0300

    Программу, по айпи возвращающая страну

--- Ip-Api_request.py
+++ Ip-Api_request.py
@@ -1,1 +1,14 @@
-import request
+import socket
+import requests
+
+ip = input("Введите IP-адрес")
+
+request = requests.get(
+    f"http://ip-api.com/json/{ip}"
+)
+
+requestJson = request.json()
+if (requestJson['status'] == 'fail') :
+    print("Таĸого IP не существует")
+else : print(requestJson['country'])
+
